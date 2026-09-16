c2279e16b040a02ef2f4e687ade7e559a3b9d6ee
zongze6688 <zhaozongze810@gmail.com>
2026-09-16 14:28:05 +0800

    added a new feature

--- main.c
+++ main.c
@@ -4,5 +4,6 @@ int main()
 {
     // @TODO: print a sentence you want.
     printf("JinCheng and all the TAs, we love you!!!!!\n\n");
+    printf("Here is a new feature.\n");
     printf("Hello, world!\n");
 }
